064f552dcce0aaa034145342cdc9851f00534e7e
Dorphia <alex.rybak333@gmail.com>
2024-05-31 14:30:25 -0400

    Finished database.js additions.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -35,6 +35,18 @@ export const putDb = async (content) => {
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    console.log("Obtaining all database notes!");
+
+    const jateDb = await openDB("jate", 1);
+    const jx = jateDb.transaction("jate", "readonly");
+      const store = jx.objectStore("jate");
+      const request = store.get(1);
+      const result = await request;
+      result
+        ? console.log("Notes obtained from database:", result.value)
+        : console.log("No notes have been found...");
+      return result?.value;
+}
 
 initdb();
